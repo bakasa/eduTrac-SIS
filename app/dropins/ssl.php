9b298007268028ab702c2c194ec38c96e94d681b
bakasa <bakasa@gmail.com>
2020-12-23 16:53:50 +0200

    Force all content over SSL / Https

--- app/dropins/ssl.php
+++ app/dropins/ssl.php
@@ -1,15 +1,22 @@
 <?php
 /*
-  DropIn Name: Enable SSL
-  Description: A dropin which tranfers all HTTP requests over SSL.
+  DropIn Name: Enable and Force SSL
+  Description: A dropin which loads the entire eduTrac SIS installation over HTTPS.
  */
 
 $app = \Liten\Liten::getInstance();
 
-function enable_url_ssl()
+function enable_force_url_ssl()
 {
+    $app = \Liten\Liten::getInstance();
     $domain = str_replace('http://', '', url('/'));
     $url = 'https://' . $domain;
+
+    if ($app->req->server['SERVER_PORT'] != 443) {
+        $https = rtrim($url, '/');
+        redirect($https . $app->req->server['REQUEST_URI']);
+        exit();
+    }
     return $url;
 }
-$app->hook->add_filter('base_url', 'enable_url_ssl');
+$app->hook->add_filter('base_url', 'enable_force_url_ssl');
