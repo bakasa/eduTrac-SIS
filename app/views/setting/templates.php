8ad9ce5f752d92b81732a023246e49a1df769631
bakasa <bakasa@gmail.com>
2020-12-23 12:46:14 +0200

    Updated TinyMCE from version 4 to 5

--- app/views/setting/templates.php
+++ app/views/setting/templates.php
@@ -16,7 +16,7 @@ $app->view->block('dashboard');
 $screen = 'setting';
 ?>
 
-<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+<script src="//cdn.tiny.cloud/1/w02wxbjkmqdhw92yugsag8popaw2enx9ktbo29ajd7mji37o/tinymce/5/tinymce.min.js"></script>
 <script type="text/javascript">
 tinymce.init({
 	selector: "textarea",
